9bc393bf984603dfbc0394a585d3e12beb84942e
mateuscoelho07 <mateuscoelhosola@gmail.com>
2023-10-05 11:26:40 -0300

    inclusao
    inclusao do header e footer

---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<?php
+require_once ("herpers/url.php");
+?>
+
+<link rel="stylesheet" href="<?=$BASE_URL ?>/css/style.css"></link>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        <a href="<?=$BASE_URL ?>"id="logo">
+            <img src="<?=$BASE_URL?>/img/log.svg" alt="Logo Blog">
+    </a>
+    <ul id="navbar">
+        <li><a href="<?=$BASE_URL ?>"class="nav-link">HOME</a></li>
+        <li><a href="#" class="nav-link">Categorias</a></li>
+        <li><a href="#" class="nav-link">Sobre</a></li>
+        <li><a href="<?=$BASE_URL ?>/contato.php" class="nav-link">Contato</a></li>
+    </ul>
+</header>
+</body>
